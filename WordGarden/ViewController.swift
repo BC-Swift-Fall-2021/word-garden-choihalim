03a71b08f2cf2645f353a7cc9d0430948aead2cc
Halim Choi <choizk@bc.edu>
2021-09-12 21:58:18 -0400

    Initial Commit

---
+++ WordGarden/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordGarden
+//
+//  Created by 최하림 on 9/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
